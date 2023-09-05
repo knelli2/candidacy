a6b0f43b353a1a0e6c2b1dac68f61b5a0d11a781
Kyle Nelli <knelli@caltech.edu>
2023-09-05 11:12:41 -0700

    Add global state images

--- notes.md
+++ notes.md
@@ -1,11 +1,11 @@
-Need the following frames for synchronous stepping:
+## Need the following frames for synchronous stepping:
 
 1. Level 0, all white ![](images/sync_01.png =200x200)
 2. Level 1, all green ![](images/sync_02.png =200x200)
 3. Level 1, element 9 red, rest green ![](images/sync_03.png =200x200)
 4. Level 1, all red ![](images/sync_04.png =200x200)
 
-Need the following frames for asynchronous stepping:
+## Need the following frames for asynchronous stepping:
 
 1. Level 0, all white ![](images/async_01.png =200x200)
 2. Level 1, all green ![](images/async_02.png =200x200)
@@ -19,7 +19,22 @@ Need the following frames for asynchronous stepping:
 10. Rest green level 5 ![](images/async_10.png =200x200)
 11. Element 1 red level 5 (end) ![](images/async_11.png =200x200)
 
-Need following frames for meshes:
+## Need the following for global state
+
+1. Green flat top level 2 with global state 3 ![](images/global_state_01.png =240x200)
+2. Green flat top level 3 with global state 3 ![](images/global_state_02.png =240x200)
+3. Red flat top level 3 with global state 3 ![](images/global_state_03.png =240x200)
+4. Green flat top level 3 with global state 6 ![](images/global_state_04.png =240x200)
+5. Green flat top level 4 with global state 6 ![](images/global_state_05.png =240x200)
+6. Green staggered level 5 with global state at 3 (coords -1.5,1.0)
+   ![](images/global_state_06.png =240x200)
+7. Elements 9, 6, 8 staggered green level 3, rest red level 3, with global state 3
+   ![](images/global_state_07.png =240x200)
+8. Green staggered level 3, with global state 6 ![](images/global_state_08.png =240x200)
+9. Green staggered level 4, with global state 6 ![](images/global_state_09.png =240x200)
+
+
+## Need following frames for meshes:
 
 1. Single large mesh ![](images/together.png =200x200)
 2. Exploded 9 meshes no numbers ![](images/exploded_no_numbers.png =200x200)
--- plotters/mayavi_off_screen.py
+++ plotters/mayavi_off_screen.py
@@ -25,22 +25,22 @@ def start(name: str, frame: int):
     return fig, filename
 
 
-def save(image_name: str, do_print=True):
+def save(image_name: str, do_print=True, size=image_size):
     if not image_name.endswith(".png"):
         image_name += ".png"
     mlab.options.offscreen = True
-    mlab.savefig(os.path.join(image_dir, image_name), size=image_size)
+    mlab.savefig(os.path.join(image_dir, image_name), size=size)
     if do_print:
         print("done")
 
 
-def save_close(image_name: str):
-    save(image_name=image_name)
+def save_close(image_name: str, size=image_size):
+    save(image_name=image_name, size=size)
     mlab.close()
 
 
-def save_clear(image_name: str):
-    save(image_name=image_name)
+def save_clear(image_name: str, size=image_size):
+    save(image_name=image_name, size=size)
     mlab.clf()
 
 
@@ -60,7 +60,7 @@ def test_off_screen_render():
     os.remove(os.path.join(image_dir, "blah.png"))
 
 
-def sync_async_frame_flat_top(name: str, frame: int, height: int, color: str):
+def plot_flat_top(name: str, frame: int, height: int, color: tuple):
     fig, filename = start(name, frame)
 
     x, y, z_bottom, z_top = all_flat_top_from_bottom(
@@ -76,11 +76,11 @@ def sync_async_frame_flat_top(name: str, frame: int, height: int, color: str):
 
 
 def sync_async_frame_1(name: str):
-    sync_async_frame_flat_top(name, 1, 0, white)
+    plot_flat_top(name, 1, 0, white)
 
 
 def sync_async_frame_2(name: str):
-    sync_async_frame_flat_top(name, 2, 1, green)
+    plot_flat_top(name, 2, 1, green)
 
 
 def sync_async_split_colors(
@@ -112,7 +112,7 @@ def sync_async_frame_3(name: str):
 
 
 def sync_frame_4():
-    sync_async_frame_flat_top("sync", 4, 1, red)
+    plot_flat_top("sync", 4, 1, red)
 
 
 def async_frame_4():
@@ -157,22 +157,193 @@ def async_frame_10():
     )
 
 
-def async_frame_11():
-    fig, filename = start("async", 11)
+def plot_staggered(
+    name: str, frame: int, level: int, color: tuple, size: tuple = image_size
+):
+    fig, filename = start(name, frame)
 
-    x, y, z_bottom, z_top = staggered_height_from_bottom(level=5, squash_factor=squash)
+    x, y, z_bottom, z_top = staggered_height_from_bottom(
+        level=level, squash_factor=squash
+    )
 
-    bar = mlab.barchart(x, y, z_bottom, z_top, color=red, figure=fig)
+    bar = mlab.barchart(x, y, z_bottom, z_top, color=color, figure=fig)
     set_line_width(bar)
 
     set_scene()
 
-    save_clear(filename)
+    save_clear(filename, size=size)
+
+
+def async_frame_11():
+    plot_staggered(name="async", frame=11, level=5, color=red)
+
+
+global_state_size = (1200, 1000)
+
+
+def plot_global_state_bar_no_save(fig, height: int, color: tuple):
+    x = np.array([-1.25])
+    y = np.array([1.0])
+    z_bottom = np.array([0.0])
+    z_top = squash * np.array([height])
+    bar = mlab.barchart(x, y, z_bottom, z_top, color=color, figure=fig)
+    set_line_width(bar)
+
+
+def plot_staggered_with_global_state(
+    frame: int,
+    level: int,
+    global_state_height: int,
+    global_state_color: tuple,
+):
+    fig, filename = start("global_state", frame)
+
+    x, y, z_bottom, z_top = staggered_height_from_bottom(
+        level=level, squash_factor=squash
+    )
+
+    bar = mlab.barchart(x, y, z_bottom, z_top, color=green, figure=fig)
+    set_line_width(bar)
+
+    plot_global_state_bar_no_save(
+        fig, height=global_state_height, color=global_state_color
+    )
+
+    set_scene()
+
+    save_clear(filename, size=global_state_size)
+
+
+def plot_flat_top_with_global_state(
+    frame: int,
+    height: int,
+    color: tuple,
+    global_state_height: int,
+    global_state_color: tuple,
+):
+    fig, filename = start("global_state", frame)
+
+    x, y, z_bottom, z_top = all_flat_top_from_bottom(
+        level=height, squash_factor=squash
+    )
+
+    bar = mlab.barchart(x, y, z_bottom, z_top, color=color, figure=fig)
+    set_line_width(bar)
+
+    plot_global_state_bar_no_save(
+        fig, height=global_state_height, color=global_state_color
+    )
+
+    set_scene()
+
+    save_clear(filename, size=global_state_size)
+
+
+def global_state_frame_1():
+    plot_flat_top_with_global_state(
+        frame=1,
+        height=2,
+        color=green,
+        global_state_height=3,
+        global_state_color=purple,
+    )
+
+
+def global_state_frame_2():
+    plot_flat_top_with_global_state(
+        frame=2,
+        height=3,
+        color=green,
+        global_state_height=3,
+        global_state_color=purple,
+    )
+
+
+def global_state_frame_3():
+    plot_flat_top_with_global_state(
+        frame=3,
+        height=3,
+        color=red,
+        global_state_height=3,
+        global_state_color=purple,
+    )
+
+
+def global_state_frame_4():
+    plot_flat_top_with_global_state(
+        frame=4,
+        height=3,
+        color=green,
+        global_state_height=6,
+        global_state_color=blue,
+    )
+
+
+def global_state_frame_5():
+    plot_flat_top_with_global_state(
+        frame=5,
+        height=4,
+        color=green,
+        global_state_height=6,
+        global_state_color=blue,
+    )
+
+
+def global_state_frame_6():
+    plot_staggered_with_global_state(
+        frame=6, level=5, global_state_height=3, global_state_color=purple
+    )
+
+
+def global_state_frame_7():
+    fig, filename = start("global_state", 7)
+
+    x, y, z_bottom, z_top = manhattan_distance_less_equal(
+        distance=1, level=3, squash_factor=squash
+    )
+    bar = mlab.barchart(x, y, z_bottom, z_top, color=green, figure=fig)
+    set_line_width(bar)
+    x, y, z_bottom, z_top = manhattan_distance_greater(
+        distance=1, level=3, squash_factor=squash
+    )
+    bar = mlab.barchart(x, y, z_bottom, z_top, color=red, figure=fig)
+    set_line_width(bar)
+
+    plot_global_state_bar_no_save(fig, height=3, color=purple)
+
+    set_scene()
+
+    save_clear(filename, size=global_state_size)
+
+
+def global_state_frame_8():
+    plot_staggered_with_global_state(
+        frame=8, level=3, global_state_height=6, global_state_color=blue
+    )
+
+
+def global_state_frame_9():
+    plot_staggered_with_global_state(
+        frame=9, level=4, global_state_height=6, global_state_color=blue
+    )
 
 
 # For some reason the first image doesn't render properly so we put a dummy
 # image here instead
 test_off_screen_render()
+
+# Global state
+global_state_frame_1()
+global_state_frame_2()
+global_state_frame_3()
+global_state_frame_4()
+global_state_frame_5()
+global_state_frame_6()
+global_state_frame_7()
+global_state_frame_8()
+global_state_frame_9()
+
+exit()
 
 # Async
 sync_async_frame_1("async")
